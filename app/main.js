02319a34128efa829ab77550f9a3dc17c96da212
Lee Bock <lbock@esri.com>
2013-04-02 11:54:40 -0400

    1) created a main.js 2) moved images to resources directory 3)

---
+++ app/main.js
@@ -0,0 +1,92 @@
+dojo.require("dijit.layout.BorderContainer");
+dojo.require("dijit.layout.ContentPane");
+dojo.require("esri.arcgis.utils");
+dojo.require("esri.map");
+
+/******************************************************
+***************** begin config section ****************
+*******************************************************/
+
+var TITLE = "This is the title."
+var BYLINE = "This is the byline"
+var WEBMAP_ID = "3732b8a6d0bc4a09b00247e8daf69af8";
+var GEOMETRY_SERVICE_URL = "http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer";
+
+/******************************************************
+***************** end config section ******************
+*******************************************************/
+
+var _map;
+
+var _dojoReady = false;
+var _jqueryReady = false;
+
+var _initExtent;
+
+var _isMobile = isMobile();
+
+dojo.addOnLoad(function() {_dojoReady = true;init()});
+jQuery(document).ready(function() {_jqueryReady = true;init()});
+
+
+function init() {
+	
+	if (!_jqueryReady) return;
+	if (!_dojoReady) return;
+	
+	// jQuery event assignment
+	
+	$(this).resize(handleWindowResize);
+	
+	$("#zoomIn").click(function(e) {
+        _map.setLevel(_map.getLevel()+1);
+    });
+	$("#zoomOut").click(function(e) {
+        _map.setLevel(_map.getLevel()-1);
+    });
+	$("#zoomExtent").click(function(e) {
+        _map.setExtent(_initExtent);
+    });
+	
+	$("#title").append(TITLE);
+	$("#subtitle").append(BYLINE);	
+
+	var mapDeferred = esri.arcgis.utils.createMap(WEBMAP_ID, "map", {
+		mapOptions: {
+			slider: false,
+			wrapAround180: true,
+			extent:_initExtent
+		},
+		ignorePopups: false,
+		geometryServiceURL: GEOMETRY_SERVICE_URL
+	});
+	
+	mapDeferred.addCallback(function(response) {	  
+
+		_map = response.map;
+
+		//resize the map when the browser resizes
+		dojo.connect(dijit.byId('map'), 'resize', _map, _map.resize);
+		
+		if(_map.loaded){
+			initMap();
+		} else {
+			dojo.connect(_map,"onLoad",function(){
+				initMap();
+			});
+		}
+				
+	});
+	
+}
+
+function initMap() {
+	if (!_initExtent) _initExtent = _map.extent;
+	handleWindowResize();	
+}
+
+function handleWindowResize() {
+	$("#map").height($("body").height() - $("#header").height());
+	$("#map").width($("body").width());
+	console.log($("body").width(), $("body").height(), $("#header").height(), $("#map").width(), $("#map").height());
+}
