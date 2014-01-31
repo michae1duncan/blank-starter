d7283791e998f8fa5f55ec03680203443cb657bd
Lee Bock <lbock@esri.com>
2014-01-31 16:30:48 -0500

    changed web map id -- original seems to be defunct

--- app/main.js
+++ app/main.js
@@ -9,7 +9,7 @@ dojo.require("esri.map");
 
 var TITLE = "This is the title."
 var BYLINE = "This is the byline"
-var WEBMAP_ID = "3732b8a6d0bc4a09b00247e8daf69af8";
+var WEBMAP_ID = "caca75ada5f14f1dad84a560db831a50";
 var GEOMETRY_SERVICE_URL = "http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer";
 
 /******************************************************
